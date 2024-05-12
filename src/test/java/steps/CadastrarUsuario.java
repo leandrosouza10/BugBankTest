785de1906cbf05879148b9fe65d8ef2688145462
Leandro Souza <souzaleandro79@gmail.com>
2024-05-11 22:31:16 -0300

    Steps estrutura cadastrar usuario

---
+++ src/test/java/steps/CadastrarUsuario.java
@@ -0,0 +1,19 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class CadastrarUsuario {
+    @Dado("que esteja na página de registrar")
+    public void queEstejaNaPáginaDeRegistrar() {
+    }
+
+    @Quando("inserir as informações")
+    public void inserirAsInformações() {
+    }
+
+    @Entao("validar a mensagem criada com sucesso")
+    public void validarAMensagemCriadaComSucesso() {
+    }
+}
